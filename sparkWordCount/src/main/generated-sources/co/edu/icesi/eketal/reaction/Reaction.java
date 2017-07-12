d32c091f534f76551667daa06ca545e16df849b3
christiansotoanaya <christian.soto-a@mail.escuelaing.edu.co>
2017-07-12 10:54:13 -0500

    Arreglo de ejecucion

--- sparkWordCount/src/main/generated-sources/co/edu/icesi/eketal/reaction/Reaction.java
+++ sparkWordCount/src/main/generated-sources/co/edu/icesi/eketal/reaction/Reaction.java
@@ -5,62 +5,6 @@ import co.edu.icesi.ketal.core.State;
 
 @SuppressWarnings("all")
 public class Reaction {
-  public static void reactionseqEventDetectordeadState() {
-    String msg = "DEAD STATE";
-    System.out.println();
-    System.out.println("----------------------------------------");
-    System.out.println("----------------------------------------");
-    System.out.println(msg);
-    System.out.println("----------------------------------------");
-    System.out.println("----------------------------------------");
-    System.out.println();
-  }
-  
-  public static void reactionseqEventDetectorinitialState() {
-    String msg = "Reaction detected initialState  -----  Cargando archivo al RDD";
-    System.out.println();
-    System.out.println("--------------------------------------------------------------------");
-    System.out.println(msg);
-    System.out.println("--------------------------------------------------------------------");
-    System.out.println();
-  }
-  
-  public static void reactionseqEventDetectorsecondState() {
-    String msg = "Reaction detected secondState  -----  Separando palabras";
-    System.out.println();
-    System.out.println("--------------------------------------------------------------------");
-    System.out.println(msg);
-    System.out.println("--------------------------------------------------------------------");
-    System.out.println();
-  }
-  
-  public static void reactionseqEventDetectorthirdState() {
-    String msg = "Reaction detected thirdState  -----  Creando tuplas por palabras";
-    System.out.println();
-    System.out.println("--------------------------------------------------------------------");
-    System.out.println(msg);
-    System.out.println("--------------------------------------------------------------------");
-    System.out.println();
-  }
-  
-  public static void reactionseqEventDetectorfourState() {
-    String msg = "Reaction detected fourState  -----  Sumando las palabras repetidas";
-    System.out.println();
-    System.out.println("--------------------------------------------------------------------");
-    System.out.println(msg);
-    System.out.println("--------------------------------------------------------------------");
-    System.out.println();
-  }
-  
-  public static void reactionseqEventDetectorfiveState() {
-    String msg = "Reaction detected fiveState  -----  Sumando las palabras repetidas";
-    System.out.println();
-    System.out.println("--------------------------------------------------------------------");
-    System.out.println(msg);
-    System.out.println("--------------------------------------------------------------------");
-    System.out.println();
-  }
-  
   public static void reactionseqEventDetectorfinalState() {
     String msg = "Reaction detected finalState  Mostrando Resultado";
     System.out.println();
@@ -72,26 +16,8 @@ public class Reaction {
   
   public static void verifyBefore(final Automaton automaton) {
     State actual = automaton.getCurrentState();
-    if(actual.equals(co.edu.icesi.eketal.automaton.SeqEventDetector.estados.get("initialState"))){
-    	reactionseqEventDetectorinitialState();
-    }
-    if(actual.equals(co.edu.icesi.eketal.automaton.SeqEventDetector.estados.get("secondState"))){
-    	reactionseqEventDetectorsecondState();
-    }
-    if(actual.equals(co.edu.icesi.eketal.automaton.SeqEventDetector.estados.get("deadState"))){
-    	reactionseqEventDetectordeadState();
-    }
-    if(actual.equals(co.edu.icesi.eketal.automaton.SeqEventDetector.estados.get("thirdState"))){
-    	reactionseqEventDetectorthirdState();
-    }
-    if(actual.equals(co.edu.icesi.eketal.automaton.SeqEventDetector.estados.get("fourState"))){
-    	reactionseqEventDetectorfourState();
-    }
     if(actual.equals(co.edu.icesi.eketal.automaton.SeqEventDetector.estados.get("finalState"))){
     	reactionseqEventDetectorfinalState();
-    }
-    if(actual.equals(co.edu.icesi.eketal.automaton.SeqEventDetector.estados.get("fiveState"))){
-    	reactionseqEventDetectorfiveState();
     }
   }
   
